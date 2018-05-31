c7f70602903c2519318582b2cc251e5d51181d14
Lothion <ba_flo@web.de>
2018-05-31 16:29:16 +0200

    added logic, time and date methods

--- src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/systemtime/SystemTimeLogic.java
+++ src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/systemtime/SystemTimeLogic.java
@@ -8,10 +8,66 @@
  */
 package de.unistuttgart.iaas.amyassist.amy.plugin.systemtime;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Service;
+
 /**
- * TODO: Description
- * @author Tim Neumann
+ * The Logic for the system time
+ * 
+ * @author Florian Bauer, Patrick Gebhardt
  */
+@Service
 public class SystemTimeLogic {
 
+	/**
+	 * 
+	 * @return object of type Date
+	 */
+	public Date getTimeStamp() {
+		return Calendar.getInstance().getTime();
+	}
+
+	public String getDay() {
+		DateFormat dayFormat = new SimpleDateFormat("dd");
+		return dayFormat.format(Calendar.getInstance().getTime()).toString();
+	}
+
+	public String getMonth() {
+		DateFormat monthFormat = new SimpleDateFormat("MM");
+		return monthFormat.format(Calendar.getInstance().getTime()).toString();
+	}
+
+	public String getYear() {
+		DateFormat yearFormat = new SimpleDateFormat("yy");
+		return yearFormat.format(Calendar.getInstance().getTime()).toString();
+	}
+
+	public String getDate() {
+		DateFormat dateFormat = new SimpleDateFormat("dd MM yy");
+		return dateFormat.format(Calendar.getInstance().getTime()).toString();
+	}
+
+	public String getHour() {
+		DateFormat hourFormat = new SimpleDateFormat("HH");
+		return hourFormat.format(Calendar.getInstance().getTime()).toString();
+	}
+
+	public String getMinute() {
+		DateFormat minuteFormat = new SimpleDateFormat("mm");
+		return minuteFormat.format(Calendar.getInstance().getTime()).toString();
+	}
+
+	public String getSecond() {
+		DateFormat secondFormat = new SimpleDateFormat("ss");
+		return secondFormat.format(Calendar.getInstance().getTime()).toString();
+	}
+
+	public String getTime() {
+		DateFormat timeFormat = new SimpleDateFormat("HH mm ss");
+		return timeFormat.format(Calendar.getInstance().getTime()).toString();
+	}
 }
--- src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/systemtime/SystemTimeSpeech.java
+++ src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/systemtime/SystemTimeSpeech.java
@@ -8,24 +8,61 @@
  */
 package de.unistuttgart.iaas.amyassist.amy.plugin.systemtime;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-import de.unistuttgart.iaas.amyassist.amy.core.Grammar;
-import de.unistuttgart.iaas.amyassist.amy.core.SpeechCommand;
+import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Reference;
+import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Service;
+import de.unistuttgart.iaas.amyassist.amy.core.plugin.api.Grammar;
+import de.unistuttgart.iaas.amyassist.amy.core.plugin.api.Init;
+import de.unistuttgart.iaas.amyassist.amy.core.plugin.api.SpeechCommand;
 
 /**
  * A plugin which tells time and date
  * 
- * @author Florian Bauer
+ * @author Florian Bauer, Patrick Gebhardt
  */
-
+@Service
 @SpeechCommand({ "what is", "tell me" })
-public class SystemTime {
-	private Calendar cal = Calendar.getInstance();
+public class SystemTimeSpeech {
 
+	@Reference
+	private SystemTimeLogic logic;
+
+	private DateFormat dateFormat = new SimpleDateFormat("dd MM yy");
+	private DateFormat timeFormat = new SimpleDateFormat("HH mm");
+
+	/**
+	 * A method which returns the current time
+	 * 
+	 * @return
+	 */
 	@Grammar("the time")
-	public void time() {
-		Calendar.getInstance().
+	public String time() {
+		Calendar cal = Calendar.getInstance();
+		String timeOut = this.timeFormat.format(cal.getTime()).toString();
+		return timeOut;
+	}
+
+	/**
+	 * A method which returns the current date
+	 * 
+	 * @return
+	 */
+	@Grammar("the date")
+	public String date() {
+		Calendar cal = Calendar.getInstance();
+		String dateOut = this.dateFormat.format(cal.getTime()).toString();
+		return dateOut;
+	}
+
+	/**
+	 * Init method
+	 */
+	@Init
+	public void init() {
+		this.logic = new SystemTimeLogic();
 	}
 
 }
