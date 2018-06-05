8ac602dfe9c0ebe9018f5bd7af266826bc01eb8c
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2018-06-05 22:06:51 +0200

    Fixes gh pages deploy script

--- scripts/deployGH-Pages.sh
+++ scripts/deployGH-Pages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir gh-pages
+mvn install
 mvn project-info-reports:dependencies
 mv target/site gh-pages/dependencies
 mv gh-pages/dependencies/dependencies.html gh-pages/dependencies/index.html
