388fae262be71035efce1819821535a83abc120e
Legion2 <leon.k97@gmx.de>
2018-06-06 14:03:49 +0200

    added CORS Header to http-server

---
+++ http-server/src/main/java/de/unistuttgart/iaas/amyassist/amy/httpserver/CORSFilter.java
@@ -0,0 +1,20 @@
+package de.unistuttgart.iaas.amyassist.amy.httpserver;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+
+public class CORSFilter implements ContainerResponseFilter {
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+			throws IOException {
+
+		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+		headers.add("Access-Control-Allow-Origin", "https://amyassist.github.io");
+	}
+
+}
--- http-server/src/main/java/de/unistuttgart/iaas/amyassist/amy/httpserver/Server.java
+++ http-server/src/main/java/de/unistuttgart/iaas/amyassist/amy/httpserver/Server.java
@@ -62,6 +62,7 @@ public class Server {
 		ResourceConfig resourceConfig = new ResourceConfig(classes);
 		resourceConfig.registerClasses(this.restResources);
 		resourceConfig.register(ThrowableExceptionMapper.class);
+		resourceConfig.register(CORSFilter.class);
 		resourceConfig.register(new AbstractBinder() {
 			@Override
 			protected void configure() {
