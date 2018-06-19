6a3230f3a7b4d2a3f8d1e57a6c987cd0763d38cd
Christian <st148858@stud.uni-stuttgart.de>
2018-06-19 11:32:14 +0200

    added missing annotations

--- http-server/src/main/java/de/unistuttgart/iaas/amyassist/amy/httpserver/cors/CORSFilter.java
+++ http-server/src/main/java/de/unistuttgart/iaas/amyassist/amy/httpserver/cors/CORSFilter.java
@@ -30,11 +30,14 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
 
+@Provider
+@PreMatching
 public class CORSFilter implements ContainerResponseFilter, ContainerRequestFilter {
 	
 	private static final String OPTIONS = "OPTIONS";
