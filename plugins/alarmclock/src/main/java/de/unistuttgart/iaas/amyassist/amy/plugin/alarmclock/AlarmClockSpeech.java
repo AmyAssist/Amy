5bb3a81bfa5954918c98a991261ac0f3cd7f8f5a
PatrickGe <pattigebhardt@googlemail.com>
2018-06-20 17:21:34 +0200

    adding hours, minutes and seconds to the timer --no tests yet

--- plugins/alarmclock/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/alarmclock/AlarmClockSpeech.java
+++ plugins/alarmclock/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/alarmclock/AlarmClockSpeech.java
@@ -42,8 +42,10 @@ public class AlarmClockSpeech {
 
 	/**
 	 * Sets new alarm with this scheme: hh:mm
-	 *
-	 * @return true if everything went well
+	 * 
+	 * @param params
+	 * 
+	 * @return params[3], params[5]
 	 */
 	@Grammar("set alarm (at|for) # oh #")
 	public String setAlarm(String[] params) {
@@ -53,14 +55,44 @@ public class AlarmClockSpeech {
 		return this.logic.setAlarm(new String[] { params[3], params[5] });
 	}
 
+	/**
+	 * Sets a new timer. You can select between hours, minutes and seconds or
+	 * combinate them
+	 * 
+	 * @param params
+	 * @return params for the timer
+	 */
 	@Grammar("set timer on [# hours] [# minutes] [# seconds]")
 	public String setTimer(String[] params) {
-		if (params.length >= 5) {
-			// TODO: implement
+		if (params.length == 9) {
+			return this.logic.setTimer(Integer.parseInt(params[3]), Integer.parseInt(params[5]),
+					Integer.parseInt(params[7]));
+		} else if (params.length == 7) {
+			if (params[4].equals("hours") && params[6].equals("minutes")) {
+				return this.logic.setTimer(Integer.parseInt(params[3]), Integer.parseInt(params[5]), 0);
+			} else if (params[4].equals("hours") && params[6].equals("seconds")) {
+				return this.logic.setTimer(Integer.parseInt(params[3]), 0, Integer.parseInt(params[7]));
+			} else if (params[4].equals("minutes") && params[6].equals("seconds")) {
+				return this.logic.setTimer(0, Integer.parseInt(params[5]), Integer.parseInt(params[7]));
+			}
+		} else if (params.length == 5) {
+			if (params[4].equals("hours")) {
+				return this.logic.setTimer(Integer.parseInt(params[3]), 0, 0);
+			} else if (params[4].equals("minutes")) {
+				return this.logic.setTimer(0, Integer.parseInt(params[5]), 0);
+			} else if (params[4].equals("seconds")) {
+				return this.logic.setTimer(0, 0, Integer.parseInt(params[3]));
+			}
 		}
 		return "";
 	}
 
+	/**
+	 * Resets all alarms or timers
+	 * 
+	 * @param params
+	 * @return resetAlarms or resetTimers
+	 */
 	@Grammar("reset (alarms|timers)")
 	public String resetAlarms(String[] params) {
 		if (params[1].equals("alarms"))
@@ -68,6 +100,12 @@ public class AlarmClockSpeech {
 		return this.logic.resetTimers();
 	}
 
+	/**
+	 * Deletes one specific alarm or timer
+	 * 
+	 * @param params
+	 * @return deleteAlarm or deleteTimer
+	 */
 	@Grammar("delete (alarm|timer) #")
 	public String deleteAlarm(String[] params) {
 		if (params[1].equals("alarm"))
@@ -75,6 +113,12 @@ public class AlarmClockSpeech {
 		return this.logic.deleteTimer(Integer.parseInt(params[2]));
 	}
 
+	/**
+	 * deactivates one specific alarm or timer
+	 * 
+	 * @param params
+	 * @return deactivateAlarm or deactivateTimer
+	 */
 	@Grammar("deactivate (alarm|timer) #")
 	public String deactivateAlarm(String[] params) {
 		if (params[1].equals("alarm"))
@@ -82,6 +126,12 @@ public class AlarmClockSpeech {
 		return this.logic.deactivateTimer(Integer.parseInt(params[2]));
 	}
 
+	/**
+	 * Activates one specific alarm or timer
+	 * 
+	 * @param params
+	 * @return activateAlarm or activateTimer
+	 */
 	@Grammar("activate (alarm|timer) #")
 	public String activateAlarm(String[] params) {
 		if (params[1].equals("alarm"))
@@ -89,6 +139,12 @@ public class AlarmClockSpeech {
 		return this.logic.activateTimer(Integer.parseInt(params[2]));
 	}
 
+	/**
+	 * gets one specific alarm or timer
+	 * 
+	 * @param params
+	 * @return getAlarm or getTimer
+	 */
 	@Grammar("get (alarm|timer) #")
 	public String getAlarm(String[] params) {
 		if (params[1].equals("alarm"))
@@ -96,6 +152,12 @@ public class AlarmClockSpeech {
 		return this.logic.getTimer(Integer.parseInt(params[2]));
 	}
 
+	/**
+	 * gets all alarms or timers
+	 * 
+	 * @param params
+	 * @return getAllAlarms or getAllTimers
+	 */
 	@Grammar("get all (alarms|timers)")
 	public String getAllAlarms(String[] params) {
 		if (params[2].equals("alarms"))
@@ -103,6 +165,12 @@ public class AlarmClockSpeech {
 		return String.join("\n", this.logic.getAllTimers());
 	}
 
+	/**
+	 * edits a specific alarm
+	 * 
+	 * @param params
+	 * @return edit Alarm
+	 */
 	@Grammar("edit alarm # to # oh #")
 	public String editAlarm(String[] params) {
 		if (Integer.parseInt(params[4]) > 23 || Integer.parseInt(params[6]) > 59)
