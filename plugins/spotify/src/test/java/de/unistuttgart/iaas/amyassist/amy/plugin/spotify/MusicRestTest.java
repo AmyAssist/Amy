f8b89a72ffebca1cd382fe18efc51cc936f98911
astyz <kayamuhi29@gmail.com>
2018-06-22 00:08:49 +0200

    added missing header

--- plugins/spotify/src/test/java/de/unistuttgart/iaas/amyassist/amy/plugin/spotify/MusicRestTest.java
+++ plugins/spotify/src/test/java/de/unistuttgart/iaas/amyassist/amy/plugin/spotify/MusicRestTest.java
@@ -1,9 +1,27 @@
-/**
+/*
+ * This source file is part of the Amy open source project.
+ * For more information see github.com/AmyAssist
  * 
+ * Copyright (c) 2018 the Amy project authors.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information see notice.md
  */
-package de.unistuttgart.iaas.amyassist.amy.plugin.spotify;
 
-import static org.junit.jupiter.api.Assertions.*;
+package de.unistuttgart.iaas.amyassist.amy.plugin.spotify;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,6 +41,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.mockito.Mockito;
 
 import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Reference;
@@ -135,10 +155,10 @@ class MusicRestTest {
 	}
 
 	/**
-	 * Test method for {@link de.unistuttgart.iaas.amyassist.amy.plugin.spotify.MusicResource#pausePlay()}.
+	 * Test method for {@link de.unistuttgart.iaas.amyassist.amy.plugin.spotify.MusicResource#pause()}.
 	 */
 	@Test
-	void testPausePlay() {
+	void testPause() {
 		Mockito.when(this.logic.pause()).thenReturn(true);
 		Response response = this.target.path("music").path("pause").request().post(null);
 
