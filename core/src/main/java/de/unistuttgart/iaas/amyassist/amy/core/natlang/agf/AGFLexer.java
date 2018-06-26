daf740fffb21d9a585a0af06765d5bcf5d7d36f5
Felix B <felix.burk@googlemail.com>
2018-06-26 13:03:10 +0200

    added the natlang and natlang.agf package, some interfaces and the first implementation of the lexer

---
+++ core/src/main/java/de/unistuttgart/iaas/amyassist/amy/core/natlang/agf/AGFLexer.java
@@ -0,0 +1,85 @@
+/*
+ * This source file is part of the Amy open source project.
+ * For more information see github.com/AmyAssist
+ * 
+ * Copyright (c) 2018 the Amy project authors.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information see notice.md
+ */
+
+package de.unistuttgart.iaas.amyassist.amy.core.natlang.agf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * AGF lexer implementation
+ * @author Felix Burk
+ */
+public class AGFLexer implements IAGFLexer {
+
+	/**
+	 * @see de.unistuttgart.iaas.amyassist.amy.core.natlang.agf.IAGFLexer#tokenize(java.lang.String)
+	 */
+	@Override
+	public List<AGFToken> tokenize(String toTokenize) {
+		List<AGFToken> result = new ArrayList<>();
+		
+		for(int i=0; i < toTokenize.length(); i++) {
+			char c = toTokenize.charAt(i);
+			
+			switch(c) {
+    			case '(':
+    				result.add(new AGFToken(AGFTokenType.OPENBR, "("));
+    				break;
+    			case ')':
+    				result.add(new AGFToken(AGFTokenType.CLOSEBR, ")"));
+    				break;
+    			case '|':
+    				result.add(new AGFToken(AGFTokenType.OR, "|"));
+    				break;
+    			case '[':
+    				result.add(new AGFToken(AGFTokenType.OPENSBR, "["));
+    				break;
+    			case ']':
+    				result.add(new AGFToken(AGFTokenType.CLOSESBR, "]"));
+    				break;
+    			case '#':
+    				result.add(new AGFToken(AGFTokenType.RULE, "#"));
+    				break;
+    			default: 
+    				//only allow normal SPACE codepoint 32, U+0020
+    				if(c == 32) {
+    					break;
+    				}else if(Character.isLetter(c)) {
+    					StringBuilder builder = new StringBuilder();
+    					while(i < toTokenize.length() && Character.isLetter(toTokenize.charAt(i))) {
+    						builder.append(toTokenize.charAt(i));
+    						i++;
+    					}
+    					result.add(new AGFToken(AGFTokenType.WORD, builder.toString()));
+    					i--;
+    				}else {
+    					throw new AGFLexerException("wrong character " + c);
+    				}
+    				break;
+							}
+		}
+		return result;
+	}
+
+}
