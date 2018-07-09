782682aa07bd91ec62666783055963bc690db4e4
Christian <st148858@stud.uni-stuttgart.de>
2018-07-09 15:39:25 +0200

    added new project for utilities , added base classes for rest resources and entities

---
+++ utility/src/main/java/de/unistuttgart/iaas/amyassist/amy/utility/rest/Resource.java
@@ -0,0 +1,40 @@
+/*
+ * This source file is part of the Amy open source project.
+ * For more information see github.com/AmyAssist
+ * 
+ * Copyright (c) 2018 the Amy project authors.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information see notice.md
+ */
+
+package de.unistuttgart.iaas.amyassist.amy.utility.rest;
+
+/**
+ * Interface for Resource Classes
+ * 
+ * @author Christian Bräuner
+ */
+public interface Resource {
+
+	/**
+	 * returns a description for a this plugin, that contains information
+	 * about provided methods and needed parameters
+	 * 
+	 * @return the enitity representing the plugin
+	 */
+	public ResourceEntity getPluginDescripion();
+}
