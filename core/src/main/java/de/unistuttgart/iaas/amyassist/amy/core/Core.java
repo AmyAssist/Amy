c463b56446b520f35bc57be4a77050c8c7b8bbac
Benno <benno@bensge.com>
2018-07-12 21:14:51 +0200

    New AbstractRegistryResource & ContactRegistryResource

--- core/src/main/java/de/unistuttgart/iaas/amyassist/amy/core/Core.java
+++ core/src/main/java/de/unistuttgart/iaas/amyassist/amy/core/Core.java
@@ -29,6 +29,7 @@ import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import de.unistuttgart.iaas.amyassist.amy.registry.rest.ContactRegistryResource;
 import de.unistuttgart.iaas.amyassist.amy.registry.rest.LocationRegistryResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -119,6 +120,7 @@ public class Core {
 		this.server = this.di.getService(Server.class);
 		this.server.register(HomeResource.class);
 		this.server.register(LocationRegistryResource.class);
+		this.server.register(ContactRegistryResource.class);
 
 		initConsole();
 
