026ba719b81ed34554085eb23879cbc5a0a0737d
astyz <kayamuhi29@gmail.com>
2018-07-21 18:50:00 +0200

    added rest options for alarmclock

--- plugins/alarmclock/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/alarmclock/AlarmClockResource.java
+++ plugins/alarmclock/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/alarmclock/AlarmClockResource.java
@@ -30,6 +30,7 @@ import java.util.NoSuchElementException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -44,8 +45,10 @@ import javax.ws.rs.core.UriInfo;
 import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Reference;
 import de.unistuttgart.iaas.amyassist.amy.plugin.alarmclock.rest.Timestamp;
 import de.unistuttgart.iaas.amyassist.amy.utility.rest.Method;
+import de.unistuttgart.iaas.amyassist.amy.utility.rest.Parameter;
 import de.unistuttgart.iaas.amyassist.amy.utility.rest.Resource;
 import de.unistuttgart.iaas.amyassist.amy.utility.rest.ResourceEntity;
+import de.unistuttgart.iaas.amyassist.amy.utility.rest.Types;
 
 /**
  * REST Resource for alarmclock
@@ -64,7 +67,7 @@ public class AlarmClockResource implements Resource {
 	private AlarmClockLogic logic;
 
 	@Context
-	private UriInfo uri;
+	private UriInfo info;
 
 	/**
 	 * returns all alarms
@@ -156,7 +159,7 @@ public class AlarmClockResource implements Resource {
 	}
 
 	/**
-	 * sets a alarm to a given timestamp
+	 * sets an alarm to a given timestamp
 	 * 
 	 * @param alarmTime
 	 *            the timestamp for the alarm
@@ -185,7 +188,7 @@ public class AlarmClockResource implements Resource {
 	}
 
 	private URI createAlarmPath(int id) {
-		return this.uri.getBaseUriBuilder().path(AlarmClockResource.class).path(AlarmClockResource.class, "getAlarm")
+		return this.info.getBaseUriBuilder().path(AlarmClockResource.class).path(AlarmClockResource.class, "getAlarm")
 				.build(Integer.valueOf(id));
 	}
 
@@ -194,7 +197,13 @@ public class AlarmClockResource implements Resource {
 	 */
 	@Override
 	public ResourceEntity getPluginDescripion() {
-		return null;
+		ResourceEntity resource = new ResourceEntity();
+		resource.setName("AlarmClock Plugin");
+		resource.setDescription("A Plugin which provides an alarm and timer functionality. "
+				+ "This plugin makes it possible to set, delete, activate, deactivate and edit alarms and timers.");
+		resource.setMethods(this.getPluginMethods());
+		resource.setLink(this.info.getBaseUriBuilder().path(AlarmClockResource.class).build());
+		return resource;
 	}
 
 	/**
@@ -202,7 +211,151 @@ public class AlarmClockResource implements Resource {
 	 */
 	@Override
 	public Method[] getPluginMethods() {
-		// TODO Auto-generated method stub
-		return null;
+		Method[] methods = new Method[5];
+		methods[0] = createGetAllAlarmsMethod();
+		methods[1] = createGetAlarmMethod();
+		methods[2] = createEditAlarmMethod();
+		methods[3] = createNewAlarmMethod();
+		methods[4] = createResetAlarmsMethod();
+		return methods;
 	}
+
+	/**
+	 * returns the method describing the getAllAlarms method
+	 * 
+	 * @return the describing method object
+	 */
+	@Path("alarms")
+	@OPTIONS
+	@Produces(MediaType.APPLICATION_JSON)
+	public Method createGetAllAlarmsMethod() {
+		Method getAll = new Method();
+		getAll.setName("Get all Alarms");
+		getAll.setDescription("Returns all alarms");
+		getAll.setLink(this.info.getBaseUriBuilder().path(AlarmClockResource.class)
+				.path(AlarmClockResource.class, "getAllAlarms").build());
+		getAll.setType(Types.GET);
+		return getAll;
+	}
+
+	/**
+	 * returns the method describing the getAlarm method
+	 * 
+	 * @return the describing method object
+	 */
+	@Path("alarms/{pathid}")
+	@OPTIONS
+	@Produces(MediaType.APPLICATION_JSON)
+	public Method createGetAlarmMethod() {
+		Method getAlarm = new Method();
+		getAlarm.setName("Get Alarm");
+		getAlarm.setDescription("Returns a specific alarm");
+		getAlarm.setLink(this.info.getBaseUriBuilder().path(AlarmClockResource.class)
+				.path(AlarmClockResource.class, "getAlarm").build());
+		getAlarm.setType(Types.GET);
+		getAlarm.setParameters(getGetAlarmParameters());
+		return getAlarm;
+	}
+
+	/**
+	 * returns the method describing the editAlarm method
+	 * 
+	 * @return the describing method object
+	 */
+	@Path("alarms/{pathid}")
+	@OPTIONS
+	@Produces(MediaType.APPLICATION_JSON)
+	public Method createEditAlarmMethod() {
+		Method editAlarm = new Method();
+		editAlarm.setName("Edit Alarm");
+		editAlarm.setDescription("Changes the properties of an alarm");
+		editAlarm.setLink(this.info.getBaseUriBuilder().path(AlarmClockResource.class)
+				.path(AlarmClockResource.class, "editAlarm").build());
+		editAlarm.setType(Types.POST);
+		editAlarm.setParameters(getEditAlarmParameters());
+		return editAlarm;
+	}
+
+	/**
+	 * returns the method describing the newAlarm method
+	 * 
+	 * @return the describing method object
+	 */
+	@Path("alarms/new")
+	@OPTIONS
+	@Produces(MediaType.APPLICATION_JSON)
+	public Method createNewAlarmMethod() {
+		Method newAlarm = new Method();
+		newAlarm.setName("New Alarm");
+		newAlarm.setDescription("Sets an alarm to a given timestamp");
+		newAlarm.setLink(this.info.getBaseUriBuilder().path(AlarmClockResource.class)
+				.path(AlarmClockResource.class, "newAlarm").build());
+		newAlarm.setType(Types.POST);
+		newAlarm.setParameters(getNewAlarmParameters());
+		return newAlarm;
+	}
+
+	/**
+	 * returns the method describing the resetAlarms method
+	 * 
+	 * @return the describing method object
+	 */
+	@Path("alarms/reset")
+	@OPTIONS
+	@Produces(MediaType.APPLICATION_JSON)
+	public Method createResetAlarmsMethod() {
+		Method resetAlarms = new Method();
+		resetAlarms.setName("Delete all Alarms");
+		resetAlarms.setDescription("Deletes all alarms");
+		resetAlarms.setLink(this.info.getBaseUriBuilder().path(AlarmClockResource.class)
+				.path(AlarmClockResource.class, "resetAlarms").build());
+		resetAlarms.setType(Types.POST);
+		return resetAlarms;
+	}
+	
+	private Parameter[] getGetAlarmParameters() {
+		Parameter[] params = new Parameter[1];
+		// pathid
+		params[0] = new Parameter();
+		params[0].setName("PathID");
+		params[0].setRequired(true);
+		params[0].setParamType(Types.PATH);
+		params[0].setValueType(Types.INTEGER);
+		return params;
+	}
+	
+	private Parameter[] getEditAlarmParameters() {
+		Parameter[] params = new Parameter[3];
+		// pathid
+		params[0] = new Parameter();
+		params[0].setName("PathID");
+		params[0].setRequired(true);
+		params[0].setParamType(Types.PATH);
+		params[0].setValueType(Types.INTEGER);
+		// mode
+		params[1] = new Parameter();
+		params[1].setName("Mode");
+		params[1].setRequired(true);
+		params[1].setParamType(Types.QUERY);
+		params[1].setValueType(Types.INTEGER);
+		// alarmTime
+		params[2] = new Parameter();
+		params[2].setName("AlarmTime");
+		params[2].setRequired(true);
+		params[2].setParamType(Types.BODY);
+		params[2].setValueType(Types.BODY); // TODO timestamp type
+		return params;
+	}
+	
+	private Parameter[] getNewAlarmParameters() {
+		Parameter[] params = new Parameter[1];
+		// alarmTime
+		params[0] = new Parameter();
+		params[0].setName("AlarmTime");
+		params[0].setRequired(true);
+		params[0].setParamType(Types.BODY);
+		params[0].setValueType(Types.BODY); // Same here
+		return params;
+	}
+	
 }
