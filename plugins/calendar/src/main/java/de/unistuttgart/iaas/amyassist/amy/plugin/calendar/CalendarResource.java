218fce84455133510a089a7c51a14210d706315c
astyz <kayamuhi29@gmail.com>
2018-07-23 19:23:18 +0200

    bugfix and removed unused lines of pom

--- plugins/calendar/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/calendar/CalendarResource.java
+++ plugins/calendar/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/calendar/CalendarResource.java
@@ -26,8 +26,8 @@ package de.unistuttgart.iaas.amyassist.amy.plugin.calendar;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
