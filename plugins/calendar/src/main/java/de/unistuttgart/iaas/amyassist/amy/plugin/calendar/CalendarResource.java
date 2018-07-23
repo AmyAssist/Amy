195a341b49842847eb0033d94d78edcd2b0cd331
astyz <kayamuhi29@gmail.com>
2018-07-23 17:59:33 +0200

    removed unused imports

--- plugins/calendar/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/calendar/CalendarResource.java
+++ plugins/calendar/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/calendar/CalendarResource.java
@@ -27,18 +27,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response.Status;
-
-import com.google.api.services.calendar.model.Event;
 
 import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Reference;
 
