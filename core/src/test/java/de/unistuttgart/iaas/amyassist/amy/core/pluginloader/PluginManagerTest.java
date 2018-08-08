2080f488f8fa6e17470df8e398d4c91e1bcab750
Legion2 <leon.k97@gmx.de>
2018-08-08 14:12:18 +0200

    removed NLProcessingManager from tests

--- core/src/test/java/de/unistuttgart/iaas/amyassist/amy/core/pluginloader/PluginManagerTest.java
+++ core/src/test/java/de/unistuttgart/iaas/amyassist/amy/core/pluginloader/PluginManagerTest.java
@@ -43,7 +43,6 @@ import de.unistuttgart.iaas.amyassist.amy.core.configuration.ConfigurationManage
 import de.unistuttgart.iaas.amyassist.amy.core.di.DependencyInjection;
 import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Reference;
 import de.unistuttgart.iaas.amyassist.amy.core.io.Environment;
-import de.unistuttgart.iaas.amyassist.amy.core.natlang.NLProcessingManager;
 import de.unistuttgart.iaas.amyassist.amy.core.persistence.Persistence;
 import de.unistuttgart.iaas.amyassist.amy.test.FrameworkExtension;
 import de.unistuttgart.iaas.amyassist.amy.test.TestFramework;
@@ -90,7 +89,6 @@ class PluginManagerTest {
 		this.properties.setProperty("mode", "dev");
 		when(configurationManager.getConfigurationWithDefaults("plugin.config")).thenReturn(this.properties);
 
-		this.testFramework.mockService(NLProcessingManager.class);
 		this.testFramework.mockService(Persistence.class);
 
 		this.serviceUnderTest = this.testFramework.setServiceUnderTest(PluginManagerService.class);
