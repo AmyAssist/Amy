44330337914e834c042edd29eec4b724b15ddf59
Benno <bensge@me.com>
2018-08-10 09:41:24 +0200

    Rename rest resource class

---
+++ remote-sr/src/main/java/de/unistuttgart/iaas/amyassist/amy/remotesr/SRResource.java
@@ -0,0 +1,61 @@
+/*
+ * This source file is part of the Amy open source project.
+ * For more information see github.com/AmyAssist
+ * 
+ * Copyright (c) 2018 the Amy project authors.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information see notice.md
+ */
+
+package de.unistuttgart.iaas.amyassist.amy.remotesr;
+
+import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Reference;
+import de.unistuttgart.iaas.amyassist.amy.utility.rest.Resource;
+import de.unistuttgart.iaas.amyassist.amy.utility.rest.ResourceEntity;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.sse.Sse;
+import javax.ws.rs.sse.SseEventSink;
+
+/**
+ * SSE resource for the remote SR. Chrome connects to this resource and stays connected indefinitely
+ *
+ * @author Benno Krauß
+ */
+@Path("remotesr")
+public class SRResource implements Resource {
+
+    @Reference
+    private RemoteSR sr;
+
+    @GET
+    @Path("eventstream")
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public void eventStream(@Context SseEventSink sink, @Context Sse sse) {
+
+        SSEClient newClient = new SSEClient(sink, sse);
+        sr.setClient(newClient);
+    }
+
+    public ResourceEntity getPluginDescripion() {
+        return null;
+    }
+}
