9fd7e787cee3eb0716a62f9331d38dc3f60200f9
Benno <bensge@me.com>
2018-08-11 21:30:47 +0200

    Launch Chrome browser automatically

--- remote-sr/src/main/java/de/unistuttgart/iaas/amyassist/amy/remotesr/RemoteSR.java
+++ remote-sr/src/main/java/de/unistuttgart/iaas/amyassist/amy/remotesr/RemoteSR.java
@@ -23,9 +23,17 @@
 
 package de.unistuttgart.iaas.amyassist.amy.remotesr;
 
+import de.unistuttgart.iaas.amyassist.amy.core.configuration.ConfigurationManager;
+import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.PostConstruct;
 import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Reference;
 import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Service;
+import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 /**
  * Class to initiate the remote SR
@@ -34,14 +42,81 @@ import org.slf4j.Logger;
 @Service
 public class RemoteSR {
 
+    private static final String CONFIG_NAME = "remotesr.config";
+    private static final String CHROME_DIRECTORY_CONFIG_KEY = "chrome";
+
+    private static final String MAC_CHROME_PATH = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+    private static final String WINDOWS_CHROME_PATH = "/Program Files (x86)/Google/Chrome/Application/chrome.exe";
+
     @Reference
     private Logger logger;
+
+    @Reference
+    public ConfigurationManager configurationManager;
 
     private static final String START_SR_EVENT = "START";
 
     private SSEClient client = null;
 
     private RemoteSRListener listener;
+
+    @PostConstruct
+    private void init() {
+        launchChrome();
+    }
+
+    /**
+     * launch the chrome browser, open the remote-SR url immediately and prevent
+     * microphone permission dialog by using a prepared user profile
+     */
+    private void launchChrome() {
+        try {
+            URL fileURL = getClass().getClassLoader().getResource("chrome_profile");
+            if (fileURL == null) {
+                throw new IOException("Couldn't get chrome_profile resource");
+            }
+            String file = fileURL.getFile();
+
+            String chromePath = configurationManager.getConfiguration(CONFIG_NAME).getProperty(CHROME_DIRECTORY_CONFIG_KEY);
+
+            if (chromePath == null) {
+                if (SystemUtils.IS_OS_MAC_OSX) {
+                    chromePath = MAC_CHROME_PATH;
+                } else if (SystemUtils.IS_OS_WINDOWS) {
+                    chromePath = WINDOWS_CHROME_PATH;
+                } else {
+                    logger.error("No directory for chrome installation found. Set your chrome installation directory " +
+                            "in {}.properties", CONFIG_NAME);
+                }
+            }
+
+            Process process = new ProcessBuilder(
+                chromePath,
+                "http://localhost:8080/rest/remotesr",
+                "--user-data-dir=" + file
+            ).start();
+
+            watchProcess(process, new BufferedReader(new InputStreamReader(process.getInputStream())));
+        } catch (IOException e) {
+            logger.warn("Couldn't start Chrome:", e);
+        }
+    }
+
+    private void watchProcess(Process process, BufferedReader reader) {
+        new Thread(() -> {
+            try {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    logger.warn("Chrome: {}", line);
+                }
+                if (!process.isAlive()) {
+                    logger.warn("Chrome quit unexpectedly");
+                }
+            } catch (IOException e) {
+                logger.warn("Couldn't read output from Chrome:", e);
+            }
+        }).start();
+    }
 
     void setClient(SSEClient client) {
         if (this.client != null && this.client.isConnected()) {
