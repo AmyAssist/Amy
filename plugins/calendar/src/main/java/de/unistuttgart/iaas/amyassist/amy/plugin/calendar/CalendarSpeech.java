e881c7c026ee261af27e1d9a33b2fdd2f6fae913
Lars Buttgereit <st141897@stud.uni-stuttgart.de>
2018-08-21 18:45:11 +0200

    new speech class for calendar plugin

--- plugins/calendar/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/calendar/CalendarSpeech.java
+++ plugins/calendar/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/calendar/CalendarSpeech.java
@@ -23,8 +23,11 @@
 
 package de.unistuttgart.iaas.amyassist.amy.plugin.calendar;
 
+import java.util.Map;
+
 import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Reference;
-import de.unistuttgart.iaas.amyassist.amy.core.natlang.api.Grammar;
+import de.unistuttgart.iaas.amyassist.amy.core.natlang.api.EntityData;
+import de.unistuttgart.iaas.amyassist.amy.core.natlang.api.Intent;
 import de.unistuttgart.iaas.amyassist.amy.core.natlang.api.SpeechCommand;
 
 /**
@@ -40,38 +43,25 @@ public class CalendarSpeech {
 
 	/**
 	 *
-	 * @param params
-	 *            params from the speech
+	 * @param entites
+	 *            from the speech
 	 *
 	 * @return the upcoming X events from the calendar
 	 */
-	@Grammar("what (is|are) my next [#] (event|events)")
-	public String getEvents(String[] params) {
-		String number;
-		if (params[4].contains("event")) {
-			number = "1";
-		} else {
-			number = params[4];
-		}
-		return this.calendar.getEvents(Integer.parseInt(number));
+	@Intent()
+	public String getEvents(Map<String, EntityData> entites) {
+		return this.calendar.getEvents(entites.get("number").getNumber());
 	}
 
 	/**
-	 * @param params
-	 *            params from the speech
+	 * @param entites
+	 *            from the speech
 	 * @return upcoming events today or tomorrow depending on input
 	 */
-	@Grammar("[which] events [do i have] (today|tomorrow)")
-	public String getEventsToday(String[] params) {
-		int position = 1;
-		if (params[0].contains("which") && params[2].contains("do")) {
-			position = 5;
-		} else if (params[0].contains("which")) {
-			position = 2;
-		} else if (params[1].contains("do")) {
-			position = 4;
-		}
-		if (params[position].contains("today")) {
+	@Intent()
+	public String getEventsToday(Map<String, EntityData> entites) {
+
+		if (entites.get("day").getString().equalsIgnoreCase("today")) {
 			return this.calendar.getEventsToday();
 		}
 		return this.calendar.getEventsTomorrow();
