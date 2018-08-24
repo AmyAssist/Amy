55194e96ccc5671bf5b02b51240eb5e77b9058bc
Felix B <felix.burk@googlemail.com>
2018-08-24 16:31:38 +0200

    fixed import mistake

--- rest-resources/src/main/java/de/unistuttgart/iaas/amyassist/amy/restresources/home/HomeResource.java
+++ rest-resources/src/main/java/de/unistuttgart/iaas/amyassist/amy/restresources/home/HomeResource.java
@@ -26,6 +26,7 @@ package de.unistuttgart.iaas.amyassist.amy.restresources.home;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
