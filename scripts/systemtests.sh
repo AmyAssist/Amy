aa940f81e53040367ddf16dde4f3c913c58c5b47
Legion2 <leon.k97@gmx.de>
2018-09-01 20:18:25 +0200

    added start up order to docker-compose

--- scripts/systemtests.sh
+++ scripts/systemtests.sh
@@ -7,8 +7,6 @@ docker-compose up -d
 sleep 5
 curl -I localhost
 docker-compose stop
-
-echo system test log:
 docker-compose logs master-node
 ERRORS=$(docker-compose logs master-node | grep -i -E -c "(Error|Exception)")
 exit $ERRORS
