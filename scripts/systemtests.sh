662ec8f6ebcccac40ce884dd157b2d8128977d06
Legion2 <leon.k97@gmx.de>
2018-09-01 19:52:21 +0200

    use docker-compose for system tests

--- scripts/systemtests.sh
+++ scripts/systemtests.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-docker run -d -p 1883:1883 -p 9001:9001 --name=broker eclipse-mosquitto
-docker run -d -p 80:80 -e "AMY_MQTT_CONFIG_BROKERADDRESS=tcp://broker" --name=system-test amy-master-node
-sleep 10
+docker-compose up -d
+sleep 5
 curl -I localhost
-docker stop system-test
-docker stop broker
+docker-compose stop
+
 echo system test log:
-docker logs system-test
-docker logs system-test | grep -i -E "(Error|Exception)"
-ERROR=$?
-if [ $ERROR -eq "0" ]; then
-	exit 1;
-else
-	exit 0;
-fi
+docker-compose logs master-node
+ERRORS=$(docker-compose logs master-node | grep -i -E -c "(Error|Exception)")
+exit $ERRORS
