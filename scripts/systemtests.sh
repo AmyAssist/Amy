a4785f115be96f14d1cc26359d6831d3ba664dd0
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2018-09-02 10:41:50 +0200

    Improve systemtest script.

--- scripts/systemtests.sh
+++ scripts/systemtests.sh
@@ -2,11 +2,19 @@
 absPath=$(readlink -f "$0")
 absDir=$(dirname "$absPath")
 cd "${absDir}"
-
 docker-compose up -d
-sleep 5
-curl -I localhost || exit 1
+sleep 8
+curl -I localhost
+curlCode=$?
 docker-compose stop
 docker-compose logs master-node
-ERRORS=$(docker-compose logs master-node | grep -i -E -c "(Error|Exception)")
-exit $ERRORS
+contCode=$(docker inspect -f '{{.State.ExitCode}}' vibrant_elbakyan)
+errorCount=$(docker-compose logs master-node | grep -i -E -c "(Error|Exception)")
+
+if [ $curlCode -ne 0 ] ;then echo "Curl:$curlCode" ;fi
+if [ $contCode -ne 0 ] ;then echo "Container:$contCode" ;fi
+if [ $errorCount -ne 0 ] ;then echo "Error count:$errorCount" ;fi
+
+#Don't include container code, because Amy get's killed by docker-compose stop, which causes a code != 0
+if [ $curlCode -ne 0 ] || [ $errorCount -ne 0 ] ;then exit 1 ;fi
+
