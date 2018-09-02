e9f395ddc936db0355b79aeae495062c3c031c77
Lars Buttgereit <st141897@stud.uni-stuttgart.de>
2018-09-02 15:15:41 +0200

    added Test annotation to test

--- core/src/test/java/de/unistuttgart/iaas/amyassist/amy/core/natlang/languageSpecific/en/EnglishContractionTest.java
+++ core/src/test/java/de/unistuttgart/iaas/amyassist/amy/core/natlang/languageSpecific/en/EnglishContractionTest.java
@@ -27,6 +27,8 @@ import de.unistuttgart.iaas.amyassist.amy.core.natlang.languagespecifics.en.Engl
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Test class for English Contraction
  * 
@@ -35,10 +37,12 @@ import static org.hamcrest.Matchers.equalTo;
 public class EnglishContractionTest {
 	private EnglishContraction contraction = new EnglishContraction();
 
+	@Test
 	void testIAm() {
 		assertThat(this.contraction.disassemblingContraction("Hello i'm bla"), equalTo("Hello i am bla"));
 	}
 
+	@Test
 	void testNot() {
 		assertThat(this.contraction.disassemblingContraction("Hello ain't bla"), equalTo("Hello am not bla"));
 		assertThat(this.contraction.disassemblingContraction("Hello won't bla"), equalTo("Hello will not bla"));
@@ -46,33 +50,39 @@ public class EnglishContractionTest {
 		assertThat(this.contraction.disassemblingContraction("Hello don't bla"), equalTo("Hello do not bla"));
 	}
 
+	@Test
 	void testLetUs() {
 		assertThat(this.contraction.disassemblingContraction("Hello let's bla"), equalTo("Hello let us bla"));
 	}
 
+	@Test
 	void testAre() {
 		assertThat(this.contraction.disassemblingContraction("you're bla"), equalTo("you are bla"));
 	}
 
+	@Test
 	void testHave() {
 		assertThat(this.contraction.disassemblingContraction("you've bla"), equalTo("you have bla"));
 	}
 
+	@Test
 	void testWill() {
 		assertThat(this.contraction.disassemblingContraction("you'll bla"), equalTo("you will bla"));
 	}
 
+	@Test
 	void testS() {
 		assertThat(this.contraction.disassemblingContraction("you's bla"), equalTo("you s bla"));
 	}
 
+	@Test
 	void testD() {
 		assertThat(this.contraction.disassemblingContraction("you'd bla"), equalTo("you d bla"));
 	}
 
+	@Test
 	void testWithMore() {
 		assertThat(this.contraction.disassemblingContraction("you'd bla ain't won't you're"),
 				equalTo("you d bla am not will not you are"));
-
 	}
 }
