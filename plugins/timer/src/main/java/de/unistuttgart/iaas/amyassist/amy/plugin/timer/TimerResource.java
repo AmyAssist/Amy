fba6b0246d5c988a1f7c272bfb1a073d54e399cf
PatrickGe <pattigebhardt@googlemail.com>
2018-09-07 12:40:06 +0200

    timer resource and test (WIP)

--- plugins/timer/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/timer/TimerResource.java
+++ plugins/timer/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/timer/TimerResource.java
@@ -24,9 +24,13 @@
 package de.unistuttgart.iaas.amyassist.amy.plugin.timer;
 
 import java.net.URI;
+import java.util.List;
+import java.util.NoSuchElementException;
 
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
 import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Reference;
@@ -44,13 +48,110 @@ public class TimerResource implements Resource {
 	/**
 	 * the resource path for this plugin
 	 */
-	public static final String PATH = "clock";
+	public static final String PATH = "timer";
 
 	@Reference
 	private TimerLogic logic;
 
 	@Context
 	private UriInfo uri;
+
+	/**
+	 * returns all timers
+	 * 
+	 * @return all timers
+	 */
+	@GET
+	@Path("timers")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Timer> getAllTimers() {
+		List<Timer> timerList = this.logic.getAllTimers();
+		for (Timer t : timerList) {
+			t.setLink(createTimerPath(t.getId()));
+		}
+		return this.logic.getAllTimers();
+	}
+
+	/**
+	 * returns a specific timer
+	 * 
+	 * @param timerNumber
+	 *            the requested timer
+	 * @return the specific timer
+	 */
+	@GET
+	@Path("timers/{pathid}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Timer getTimer(@PathParam("pathid") int timerNumber) {
+		Timer timer;
+		try {
+			timer = this.logic.getTimer(timerNumber);
+		} catch (NoSuchElementException e) {
+			throw new WebApplicationException("there is no timer" + timerNumber, e, Status.NOT_FOUND);
+		}
+		timer.setLink(createTimerPath(timerNumber));
+		return timer;
+	}
+
+	/**
+	 * sets a timer to a given timestamp
+	 * 
+	 * @param timer
+	 *            the timestamp for the timer
+	 * @return the newly created timer
+	 */
+	@POST
+	@Path("timers/new")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Timer newTimer(Timer timer) {
+		timer.setLink(createTimerPath(timer.getId()));
+		return this.logic.setTimer(timer.getTimerTime());
+	}
+
+	/**
+	 * deletes all timers
+	 */
+	@POST
+	@Path("timers/deleteAll")
+	public void deleteAllTimers() {
+		this.logic.deleteAllTimers();
+	}
+
+	/**
+	 * @param timerNumber
+	 *            number of the timer which should be deleted
+	 * @return null
+	 */
+	@POST
+	@Path("timers/delete/{pathid}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Timer deleteTimer(@PathParam("pathid") int timerNumber) {
+		this.logic.deleteTimer(timerNumber);
+		return null;
+	}
+
+	/**
+	 * @param timerNumber
+	 *            number of the timer which should be activated or deactivated
+	 * @param timerInc
+	 *            the incoming timer
+	 * @return null
+	 */
+	@POST
+	@Path("timers/de.activate/{pathid}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Timer activatedeactivateAlarm(@PathParam("pathid") int timerNumber, Timer timerInc) {
+		Timer timer = timerInc;
+		if (!timer.isActive()) {
+			this.logic.reactivateTimer(timer);
+		} else if (timer.isActive()) {
+			this.logic.pauseTimer(timer);
+		}
+		return null;
+	}
 
 	/**
 	 * @see de.unistuttgart.iaas.amyassist.amy.utility.rest.Resource#getPluginDescripion()
@@ -60,7 +161,7 @@ public class TimerResource implements Resource {
 		return null;
 	}
 
-	private URI createAlarmPath(int id) {
+	private URI createTimerPath(int id) {
 		return this.uri.getBaseUriBuilder().path(TimerResource.class).path(TimerResource.class, "getTimer")
 				.build(Integer.valueOf(id));
 	}
