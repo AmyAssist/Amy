83629e8247a981c3840e5d28485e65784762f226
Florian Bauer <ba_flo@web.de>
2018-09-14 17:21:40 +0200

    WIP new calendar event speech implementation, #439

--- plugins/calendar/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/calendar/CalendarSpeech.java
+++ plugins/calendar/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/calendar/CalendarSpeech.java
@@ -23,6 +23,9 @@
 
 package de.unistuttgart.iaas.amyassist.amy.plugin.calendar;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Map;
 
 import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Reference;
@@ -42,31 +45,79 @@ public class CalendarSpeech {
 
 	@Reference
 	private CalendarLogic calendar;
+	
+	private boolean allDay;
+	private LocalDateTime start;
+	private LocalDateTime end;
+	private LocalTime zero = LocalTime.of(0, 0, 0, 0);
+	
 
 	/**
 	 *
-	 * @param entites
+	 * @param entities
 	 *            from the speech
 	 *
 	 * @return the upcoming X events from the calendar
 	 */
 	@Intent
-	public String getEvents(Map<String, EntityData> entites) {
-		return this.calendar.getEvents(entites.get("number").getNumber());
+	public String getEvents(Map<String, EntityData> entities) {
+		return this.calendar.getEvents(entities.get("number").getNumber());
 	}
 
 	/**
-	 * @param entites
+	 * @param entities
 	 *            from the speech
 	 * @return upcoming events today or tomorrow depending on input
 	 */
 	@Intent
-	public String getEventsToday(Map<String, EntityData> entites) {
-		if (entites.get("day").getString().equalsIgnoreCase("today")) {
+	public String getEventsToday(Map<String, EntityData> entities) {
+		if (entities.get("day").getString().equalsIgnoreCase("today")) {
 			return this.calendar.getEventsToday();
 		}
 		return this.calendar.getEventsTomorrow();
 
+	}
+	
+	/**
+	 * This method creates a new event
+	 * 
+	 * @param entities
+	 * 			from the speech
+	 * @return if event could be created
+	 */
+	@Intent
+	public String createNewEvent(Map<String, EntityData> entities) {
+//		this.setAllDay(entities.get("allday").getString());
+//		this.setStartAndEnd(entities.get("startdate").getDate(), entities.get("enddate").getDate(), entities.get("starttime").getTime(), entities.get("endtime").getTime());
+//		if(this.start == null || this.end == null) {
+//			return "You have to restart the creation of a new event and please make sure that you add a time to the start and to the end if you choose an non all day event.";
+//		}
+		return "created event: " + entities.get("title").getString(); 
+	}
+	
+	private void setAllDay(String allDayString) {
+		if(allDayString.equals("yes")) {
+			this.allDay = true;
+		} else {
+			this.allDay = false;
+		}
+	}
+	
+	
+	private void setStartAndEnd(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
+		if(this.allDay) {
+			this.start = LocalDateTime.of(startDate, this.zero);
+			this.end = LocalDateTime.of(endDate, this.zero);
+		} else {
+			if(startTime != null) {
+				this.start = LocalDateTime.of(startDate, startTime);
+			} else {
+				this.start = null;
+			}
+			if(endTime != null) {
+				this.end = LocalDateTime.of(endDate, endTime);
+			}
+		}
 	}
 
 	/**
@@ -77,7 +128,7 @@ public class CalendarSpeech {
 	 */
 	@Intent
 	public String getADate(Map<String, EntityData> entities) {
-		return entities.get("date").getDate().toString();
+		return "Date: " + entities.get("date").getDate().toString();
 	}
 
 	/**
@@ -88,7 +139,7 @@ public class CalendarSpeech {
 	 */
 	@Intent
 	public String getADateTime(Map<String, EntityData> entities) {
-		return entities.get("datetime").getDateTime().toString();
+		return "DateTime: " + entities.get("datetime").getDateTime().toString();
 	}
 
 }
