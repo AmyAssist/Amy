4abcbd63bb6c9b8ca44f362ed986a837bbd26898
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2018-09-17 16:12:00 +0200

    Add configuration registry.

---
+++ plugins/tosca/src/main/java/de/unistuttgart/iaas/amyassist/amy/plugin/tosca/configurations/ConfigurationRegistry.java
@@ -0,0 +1,55 @@
+/*
+ * This source file is part of the Amy open source project.
+ * For more information see github.com/AmyAssist
+ * 
+ * Copyright (c) 2018 the Amy project authors.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information see notice.md
+ */
+
+package de.unistuttgart.iaas.amyassist.amy.plugin.tosca.configurations;
+
+import javax.annotation.Nonnull;
+
+import de.unistuttgart.iaas.amyassist.amy.core.di.annotation.Service;
+import de.unistuttgart.iaas.amyassist.amy.registry.AbstractTaggableRegistry;
+
+/**
+ * The configuration registry
+ * 
+ * @author Tim Neumann
+ */
+@Service(ConfigurationRegistry.class)
+public class ConfigurationRegistry extends AbstractTaggableRegistry<ConfiguartionEntry> {
+
+	/**
+	 * @see de.unistuttgart.iaas.amyassist.amy.registry.AbstractRegistry#getPersistenceUnitName()
+	 */
+	@Override
+	protected String getPersistenceUnitName() {
+		return "ConfigurationRegistry";
+	}
+
+	/**
+	 * @see de.unistuttgart.iaas.amyassist.amy.registry.AbstractRegistry#getEntityClass()
+	 */
+	@Override
+	protected @Nonnull Class<? extends ConfiguartionEntry> getEntityClass() {
+		return ConfiguartionEntry.class;
+	}
+
+}
